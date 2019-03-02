7a213bbd677106fe00efd722d295be46ca549cdd
UsrNameu1 <UsrNameu1@users.noreply.github.com>
2019-03-03 03:50:56 +0900

    Add randomcrop augmentation endpoint, Modify Dockerfile to make script work

---
+++ src/endpoints/augment_traindata.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+import math
+
+from cv2 import imread, imwrite
+import cv2
+from joblib import Parallel, delayed
+import click
+from loguru import logger
+
+from albumentations import RandomCrop
+
+
+@click.command()
+@click.option('--image_dir', type=click.Path(exists=True), required=True, help="aerial image directory")
+@click.option('--gt_dir', type=click.Path(exists=True), required=True, help='ground truth image directory')
+@click.option("--sample_size", type=int, required=True, help='sample size')
+@click.option("--crop_size", type=int, required=False, default=256, help='clopping size')
+@click.option("--output_dir", type=click.Path(exists=False), help='output directory')
+def augment_traindata(image_dir: str, gt_dir: str, sample_size: int, crop_size: int, output_dir: str):
+    image_dir = Path(image_dir)
+    file_names = list(path.name for path in Path(image_dir).glob('./*'))
+    gt_dir = Path(gt_dir)
+
+    image_count = len(file_names)
+    sample_size_per_image = math.ceil(sample_size / image_count)
+
+    output_dir = Path(output_dir)
+
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True)
+        output_dir.joinpath(_image_subdir_name).mkdir()
+        output_dir.joinpath(_gt_subdir_name).mkdir()
+
+    logger.debug("sample count: {}".format(image_count))
+    Parallel(n_jobs=-1)(delayed(_random_crop)(
+        image_dir=image_dir, gt_dir=gt_dir, filename=name, sample_size=sample_size_per_image, crop_size=crop_size,
+        output_dir=output_dir
+    ) for name in file_names)
+    logger.debug("done")
+
+
+_image_subdir_name = 'image'
+_gt_subdir_name = 'gt'
+
+
+def _random_crop(image_dir: Path, gt_dir: Path, filename: str, sample_size: int, crop_size: int, output_dir: Path):
+    image_path = image_dir.joinpath(filename)
+    gt_path = gt_dir.joinpath(filename)
+
+    image = imread(image_path.as_posix(), cv2.IMREAD_COLOR)
+    gt_image = imread(gt_path.as_posix(), cv2.IMREAD_GRAYSCALE)
+
+    aug = RandomCrop(width=crop_size, height=crop_size)
+    file_stem = filename.split('.')[0]
+
+    for i in range(sample_size):
+        augmented = aug(image=image, mask=gt_image)
+        image_cropped = augmented['image']
+        gt_cropped = augmented['mask']
+        imwrite(output_dir.joinpath(_image_subdir_name, '{}_{}.png'.format(file_stem, i)).as_posix(), image_cropped)
+        imwrite(output_dir.joinpath(_gt_subdir_name, '{}_{}.png'.format(file_stem, i)).as_posix(), gt_cropped)
+
+
+if __name__ == '__main__':
+    augment_traindata()
