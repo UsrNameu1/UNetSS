8f1801ca2ff6739ef316ea26c5243f0b2da44202
UsrNameu1 <UsrNameu1@users.noreply.github.com>
2019-03-16 23:49:37 +0900

    Change augment script dependency from alubumentation to imgaug

--- src/endpoints/augment_traindata.py
+++ src/endpoints/augment_traindata.py
@@ -6,10 +6,11 @@ import cv2
 from joblib import Parallel, delayed
 import click
 from loguru import logger
-from albumentations import RandomCrop
+from imgaug import augmenters as iaa
 
 from preprocess.labeling import transform_to_sizelabel
 
+iaa.AddToHueAndSaturation
 
 @click.command()
 @click.option('--image_dir', type=click.Path(exists=True), required=True, help="aerial image directory")
@@ -63,13 +64,13 @@ def _random_crop(
         gt_image = transform_to_sizelabel(gt_image)
         gt_image = cv2.cvtColor(gt_image, cv2.COLOR_RGB2BGR)
 
-    aug = RandomCrop(width=crop_size, height=crop_size)
+    aug = iaa.CropToFixedSize(width=crop_size, height=crop_size)
     file_stem = filename.split('.')[0]
 
     for i in range(sample_size):
-        augmented = aug(image=image, mask=gt_image)
-        image_cropped = augmented['image']
-        gt_cropped = augmented['mask']
+        aug_deterministic = aug.to_deterministic()
+        image_cropped = aug_deterministic.augment_image(image)
+        gt_cropped = aug_deterministic.augment_image(gt_image)
         imwrite(output_dir.joinpath(_image_subdir_name, _dummycls_name,
                                     '{}_{}.png'.format(file_stem, i)).as_posix(), image_cropped)
         imwrite(output_dir.joinpath(_gt_subdir_name, _dummycls_name,
