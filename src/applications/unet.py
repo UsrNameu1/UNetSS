fdcbc8a2b94b2432ec1972c641526935ed621016
UsrNameu1 <UsrNameu1@users.noreply.github.com>
2019-03-21 02:08:19 +0900

    Fix wrong argument for batch normalization

--- src/applications/unet.py
+++ src/applications/unet.py
@@ -42,29 +42,29 @@ def UNet(weights: str = None,
 
     # Encoding Layers
     inputs = Input(input_shape)
-    conv1 = conv_block(inputs, 64, batch_normalize)
+    conv1 = conv_block(inputs, 64, batch_normalize=batch_normalize)
     pool1 = MaxPool2D(pool_size=(2, 2))(conv1)
-    conv2 = conv_block(pool1, 128, batch_normalize)
+    conv2 = conv_block(pool1, 128, batch_normalize=batch_normalize)
     pool2 = MaxPool2D(pool_size=(2, 2))(conv2)
-    conv3 = conv_block(pool2, 256, batch_normalize)
+    conv3 = conv_block(pool2, 256, batch_normalize=batch_normalize)
     pool3 = MaxPool2D(pool_size=(2, 2))(conv3)
-    conv4 = conv_block(pool3, 512, batch_normalize)
+    conv4 = conv_block(pool3, 512, batch_normalize=batch_normalize)
     pool4 = MaxPool2D(pool_size=(2, 2))(conv4)
-    conv5 = conv_block(pool4, 1024, batch_normalize)
+    conv5 = conv_block(pool4, 1024, batch_normalize=batch_normalize)
 
     # Decoding Layers
     upconv6 = upconv_block(conv5, 512)
     merge6 = concatenate([conv4, upconv6], axis=3)
-    conv6 = conv_block(merge6, 512, batch_normalize)
+    conv6 = conv_block(merge6, 512, batch_normalize=batch_normalize)
     upconv7 = upconv_block(conv6, 256)
     merge7 = concatenate([conv3, upconv7], axis=3)
-    conv7 = conv_block(merge7, 256, batch_normalize)
+    conv7 = conv_block(merge7, 256, batch_normalize=batch_normalize)
     upconv8 = upconv_block(conv7, 128)
     merge8 = concatenate([conv2, upconv8], axis=3)
-    conv8 = conv_block(merge8, 128, batch_normalize)
+    conv8 = conv_block(merge8, 128, batch_normalize=batch_normalize)
     upconv9 = upconv_block(conv8, 64)
     merge9 = concatenate([conv1, upconv9], axis=3)
-    conv9 = conv_block(merge9, 64, batch_normalize)
+    conv9 = conv_block(merge9, 64, batch_normalize=batch_normalize)
     output = Conv2D(last_channels, 1,
                     activation='softmax',
                     padding='same',
