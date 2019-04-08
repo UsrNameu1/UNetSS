2d8345e272a8fab0e3e31a41fd7406e35b8e3d76
UsrNameu1 <UsrNameu1@users.noreply.github.com>
2019-04-08 23:52:00 +0900

    Modify train script to evaluate validation data on each epoch

--- src/endpoints/train.py
+++ src/endpoints/train.py
@@ -37,14 +37,23 @@ def train(config_path: str):
     model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
     train_root_dir = Path(configs["train_data"]["root_dir"], settings.train_subdir_name)
+    validation_root_dir = Path(configs["train_data"]["root_dir"], settings.validation_subdir_name)
     batch_size = configs["train_data"]["batch_size"]
     label = SizeLabel if configs["train_data"]["use_size_label"] else BinLabel
     train_output_branched = configs["train_data"]["branched"]
     train_data_generator = data_generator(train_root_dir, batch_size, label, train_output_branched)
+    validation_data_generator = data_generator(validation_root_dir, batch_size, label, train_output_branched)
 
-    sample_n = len(list(train_root_dir.joinpath(settings.image_subdir_name, settings.dummycls_name).glob('./*.png')))
+    train_sample_n = \
+        len(list(train_root_dir.joinpath(settings.image_subdir_name, settings.dummycls_name).glob('./*.png')))
+    validation_sample_n = \
+        len(list(validation_root_dir.joinpath(settings.image_subdir_name, settings.dummycls_name).glob('./*.png')))
     epochs = configs["train_data"]["epochs"]
-    model.fit_generator(train_data_generator, int(math.ceil(sample_n / batch_size)), epochs=epochs)
+    model.fit_generator(
+        train_data_generator, int(math.ceil(train_sample_n / batch_size)),
+        validation_data=validation_data_generator, validation_steps=validation_sample_n,
+        epochs=epochs
+    )
 
     output_dir = Path(configs["output_dir"])
     if not output_dir.exists():
